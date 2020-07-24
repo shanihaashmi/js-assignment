afa6123d915b1cd4345574a7d42af40946518c10
shanihaashmi <68010225+shanihaashmi@users.noreply.github.com>
2020-07-24 22:33:46 +0530

    Delete arrow.js

--- Day 4/arrow.js
+++
@@ -1,8 +0,0 @@
-let ask =(question,yes,no) => {if(confirm(question))yes() 
-    else no();
-}
-
-ask("do you agree?", 
-yes =>{alert("you agreed")}, 
-No =>{alert("you cancelled the execution");}
-);
