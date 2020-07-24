fbc4492dd69f0d03558678b5a1cbcb4361d37ff2
shanihaashmi <68010225+shanihaashmi@users.noreply.github.com>
2020-07-24 22:34:21 +0530

    Delete destructive.js

--- Day 4/destructive.js
+++
@@ -1,9 +0,0 @@
-const student = {
-    name: "Helsinki",
-    age: 24,
-    projects: {
-           diceGame: "Two player dice game using Javascript"
-    }
-}
-  const {name, age, projects:{diceGame}} = student;
-  console.log(`Student name is ${name} and age is ${age} and the project is ${diceGame}`);
