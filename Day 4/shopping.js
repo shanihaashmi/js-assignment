890fb4d04947288e33473ce5ed5af9721dfaeb1c
shanihaashmi <68010225+shanihaashmi@users.noreply.github.com>
2020-07-24 22:35:36 +0530

    Delete shopping.js

--- Day 4/shopping.js
+++
@@ -1,4 +0,0 @@
-let shoppingList=['Tomato', 'Potato', 'Maggie', 'Pepper'];
-console.log(shoppingList);
-let shoppingBasket=[...shoppingList,'spices', 'juice', 'cake', 'Biscuits'];
-console.log(shoppingBasket);
